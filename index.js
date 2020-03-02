e678951e859944199afd3105e0a47520c45ce875
vishsuch <60690565+vishsuch@users.noreply.github.com>
2020-03-02 17:38:01 +0530

    Bishal bhowmick
    login form and registration form along with header and footer.

--- index.js
+++ index.js
@@ -6,13 +6,11 @@ function load () {
 		document.getElementById('register').style="height:100%;";
 		document.getElementById('login').style="display:none";
 		document.getElementById('check').value="Login";
-		//document.getElementById('header').style="display:none";
 	}
 	function login () {
 		document.getElementById('register').style="display:none;";
 		document.getElementById('login').style="display:block;";
 		document.getElementById('check').value="Register";
-		//document.getElementById('header').style="display:none";
 	}
 	function check () {
 		
