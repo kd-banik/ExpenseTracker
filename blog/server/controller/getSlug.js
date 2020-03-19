5f3143826e225ea54c53873da28334f51fe0e19c
Kushaldeep kansabanik <52217913+kd-banik@users.noreply.github.com>
2020-03-19 21:11:04 +0530

    commit 6
    minute change in getSlug

--- blog/server/controller/getSlug.js
+++ blog/server/controller/getSlug.js
@@ -2,7 +2,7 @@ const connection = require("./connection");
 let getSlug = function(request,response,next){
     console.log("Slug request received ");
     console.log(request.body);
-    let sql = "SELECT COUNT(*) AS count_slug FROM tbl_blog WHERE slug_blog = '"+request.body.slug+"'";
+    let sql = "SELECT COUNT(*) AS count_slug FROM tbl_blog WHERE INSTR(slug_blog,'"+request.body.slug+"') != 0";
     connection.mysqlConnSession.query(sql,function(error , result, fields){
         if(error)
         throw error;
